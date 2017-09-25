67eeabf79270b7226f029146d27e80d6fb12fe23
Ben Weinberg <weinbergben33@gmail.com>
2017-09-25 14:40:07 +0300

    commit from jerusalem (practice overcommit)

--- Math ManiaUITests/Math_ManiaUITests.m
+++ Math ManiaUITests/Math_ManiaUITests.m
@@ -34,7 +34,7 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    int wazy = 8473;
+    int wazy = 0;
     int belz = 10;
     XCTAssertEqual(wazy, belz);
 }

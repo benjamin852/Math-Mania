e3847de46cf82e12583960bbb885c9158b16c12f
Ben Weinberg <weinbergben33@gmail.com>
2017-09-26 11:00:04 +0300

    commit from jerusalem (practice overcommit)

--- PracticeTest/PracticeTest.m
+++ PracticeTest/PracticeTest.m
@@ -28,7 +28,7 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    int wazy = 489;
+    int wazy = 489879;
     int belz = 10;
     XCTAssertEqual(wazy, belz);
 }
